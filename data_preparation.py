518b9f90bf95e52839fc0765e0e0236e65c3c6dd
User <zcemhy1@ucl.ac.uk>
2021-11-17 21:12:22 +0000

    testing

--- data_preparation.py
+++ data_preparation.py
@@ -1,1 +1,14 @@
-# prepara the data
+# prepare the data
+import pandas as pd
+
+data1 = pd.read_excel('data1.xlsx')
+# print(data1)
+# print(data1.loc[3][0])
+
+data2 = pd.read_excel('data2.xlsx')
+print(data2)
+print(data2.loc[1])
+
+data3 = pd.read_excel('data3.xlsx')
+# print(data3)
+# print(data3.loc[3][0])
