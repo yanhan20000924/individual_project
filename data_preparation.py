6637a6d7efcff0e208d71a8fb2cbebc053600668
User <zcemhy1@ucl.ac.uk>
2021-11-18 17:11:50 +0000

    all dataset

--- data_preparation.py
+++ data_preparation.py
@@ -6,9 +6,17 @@ data1 = pd.read_excel('data1.xlsx')
 # print(data1.loc[3][0])
 
 data2 = pd.read_excel('data2.xlsx')
-print(data2)
-print(data2.loc[1])
+# print(data2)
+# print(data2.loc[1])
 
 data3 = pd.read_excel('data3.xlsx')
 # print(data3)
 # print(data3.loc[3][0])
+
+data4 = pd.read_excel('data4.xlsx')
+# print(data4)
+# print(data4.loc[3][0])
+
+data5 = pd.read_excel('data5.xlsx')
+print(data5)
+print(data5.loc[3][0])
