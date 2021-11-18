e0c37f7c5cd4b6b1ea166b46d543b3cccabca577
User <zcemhy1@ucl.ac.uk>
2021-11-18 23:02:25 +0000

    all dataset

--- data_preparation.py
+++ data_preparation.py
@@ -2,20 +2,20 @@
 import pandas as pd
 
 data1 = pd.read_excel('data1.xlsx')
-# print(data1)
-# print(data1.loc[3][0])
+print(data1)
+
 
 data2 = pd.read_excel('data2.xlsx')
-# print(data2)
-# print(data2.loc[1])
+print(data2)
+
 
 data3 = pd.read_excel('data3.xlsx')
-# print(data3)
-# print(data3.loc[3][0])
+print(data3)
+
 
 data4 = pd.read_excel('data4.xlsx')
-# print(data4)
-# print(data4.loc[3][0])
+print(data4)
+
 
 data5 = pd.read_excel('data5.xlsx')
 print(data5)
